ff33b39646b9aa57420d2916d269caf302604a74
suri <swigder@gmail.com>
2015-11-03 21:01:21 -0500

    committing a basic perplexity calculator to keep the momentum going

---
+++ language_modelling/perplexity.py
@@ -0,0 +1,20 @@
+from math import pow
+
+
+class PerplexityCalculator:
+    @staticmethod
+    def calculate_probability(language_model, sentence):
+        probability = 1
+        for i in range(len(sentence)):
+            bigram = (sentence[i], sentence[i+1])
+            probability *= language_model.get_probability(bigram)
+        return probability
+
+    def calculate_sentence_perplexity(self, language_model, sentence):
+        return pow(self.calculate_probability(language_model, sentence), 1/len(sentence))
+
+    def calculate_corpus_perplexity(self, language_model, corpus):
+        perplexity = 1
+        for sentence in corpus:
+            perplexity *= self.calculate_sentence_perplexity(language_model, sentence)
+        return perplexity
