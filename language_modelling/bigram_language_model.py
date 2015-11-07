cc83a0721bf31ba0ac66c7ac3a1ed88d3a7ffb94
suri <swigder@gmail.com>
2015-11-07 16:16:44 -0500

    a perplexity calculation that works -- that's a start

--- language_modelling/bigram_language_model.py
+++ language_modelling/bigram_language_model.py
@@ -5,10 +5,19 @@ class BigramLanguageModel:
     def __init__(self, corpus):
         ngram_calculator = NgramCalculator(corpus)
         self.unigrams = ngram_calculator.calculate_ngrams(1)
-        self.bigrams = ngram_calculator.calculate_ngrams(2)
+        self.bigrams = ngram_calculator.calculate_ngrams(2, True, False)
+        self.corpus_length = len(corpus.get_sentences())
 
     def get_probability(self, bigram):
         x, y = bigram
-        probability_y_given_x = self.bigrams[bigram]
-        probability_x = self.unigrams[x]
-        return probability_y_given_x / probability_x
+        return self.get_bigram_count(bigram) / self.get_unigram_count(x)
+
+    def get_bigram_count(self, bigram):
+        return self.bigrams[bigram] if bigram in self.bigrams else 1  # log(1) = 0 so adds 0 to sum
+
+    def get_unigram_count(self, unigram):
+        if unigram == '<s>':
+            return self.corpus_length
+        if unigram in self.unigrams:
+            return self.unigrams[unigram]
+        return 1  # don't want to cause math error by returning 0; numerator will be zero anyway
--- language_modelling/perplexity.py
+++ language_modelling/perplexity.py
@@ -1,20 +1,23 @@
 from math import pow
+from math import log
+from math import exp
 
 
 class PerplexityCalculator:
     @staticmethod
-    def calculate_probability(language_model, sentence):
-        probability = 1
-        for i in range(len(sentence)):
+    def calculate_sentence_probability(language_model, sentence):
+        probability = log(language_model.get_probability(('<s>', sentence[0])))
+        for i in range(len(sentence)-1):
             bigram = (sentence[i], sentence[i+1])
-            probability *= language_model.get_probability(bigram)
+            probability += log(language_model.get_probability(bigram))
         return probability
 
-    def calculate_sentence_perplexity(self, language_model, sentence):
-        return pow(self.calculate_probability(language_model, sentence), 1/len(sentence))
-
     def calculate_corpus_perplexity(self, language_model, corpus):
-        perplexity = 1
+        entropy = 0
+        words = 0
         for sentence in corpus.get_sentences():
-            perplexity *= self.calculate_sentence_perplexity(language_model, sentence)
+            words += len(sentence)
+            entropy += self.calculate_sentence_probability(language_model, sentence)
+        entropy *= -1/words
+        perplexity = exp(entropy)
         return perplexity
